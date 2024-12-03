399b2f7abbe98a821da8023aafd148c3de7c2c2d
IlarionDub <ilariondub5@gmail.com>
2024-12-03 19:21:45 +0100

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ let users1 = JSON.parse(localStorage.getItem("users1")) || [
     { name: "Admin", email: "admin@gmail.com", password: "Admin123", role: "admin" }
 ];
 
-
+ 
 // Завантаження даних із сервера під час ініціалізації
 document.addEventListener("DOMContentLoaded", async () => {
     try {
